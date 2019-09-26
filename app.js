8f2b232dc348bd034bee652630d5fc28ecfaed33
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-09-25 23:44:32 -0600

    finish Star Wars contact app

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
-const fetchData = async (url) => {
-  let res = await fetch(url)
-  let json = await res.json();
-  return json;
+const PEOPLE_URL = "https://swapi.co/api/people/";
+
+const contacts = [];
+
+const searchField = document.querySelector(".searchField");
+const contactsList = document.querySelector(".contactsList");
+
+fetch(PEOPLE_URL)
+  .then(data => data.json())
+  .then(json => contacts.push(...json.results));
+
+console.log(contacts);
+
+// Filter matches
+function filterContacts(searchInput, contacts) {
+  return contacts.filter(contact => {
+    const regex = new RegExp(searchInput, "gi");
+    return contact.name.match(regex);
+  });
 }
 
-const ul = document.querySelector("#contactsList");
+// Display matches
+function displayMatches() {
+  const filteredArray = filterContacts(this.value, contacts);
+  const html = filteredArray
+    .map(contact => {
+      return `<li>${contact.name}</li>`;
+    })
+    .join("");
+  contactsList.innerHTML = html;
+}
 
-const loadContacts = () => {
-  let page = 1;
-  for (var i = 1; i < 10; i++) {
-    let url = `https://swapi.co/api/people/?page=${page}`
-    fetchData(url).then(json => {
-      addToDom(json.results);
-      page++;
-    });
-  };
-};
+// Add event listeners that fire function to update list
+searchField.addEventListener("keyup", displayMatches);
 
+// const fetchData = async (url) => {
+//   let res = await fetch(url)
+//   let json = await res.json();
+//   console.log(json);
+// }
 
-const addToDom = (json) => {
-  console.log(json);
-  json.forEach(person => {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(person.name));
-    ul.appendChild(li);
-  });
-};
+// const ul = document.querySelector("#contactsList");
 
-loadContacts();
+// const loadContacts = () => {
+//   let page = 1;
+//   for (var i = 1; i < 10; i++) {
+//     let url = `https://swapi.co/api/people/?page=${page}`
+//     fetchData(url).then(json => {
+//       addToDom(json.results);
+//       page++;
+//     });
+//   };
+// };
+
+// const addToDom = (json) => {
+//   console.log(json);
+//   json.forEach(person => {
+//     let li = document.createElement("li");
+//     li.appendChild(document.createTextNode(person.name));
+//     ul.appendChild(li);
+//   });
+// };
+
+// loadContacts();
